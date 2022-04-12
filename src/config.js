dc8748f32402752e560f558ad646d75a94ad1e65
KevinAndSH <kevin@uninorte.edu.co>
2022-04-12 16:15:23 -0500

    GraphQL playground added

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 const { gql } = require("apollo-server")
+const { ApolloServerPluginLandingPageGraphQLPlayground } = require("apollo-server-core")
 const jwt = require("jsonwebtoken")
 const isISBN = require("is-isbn")
 const createDataLoaders = require("./dataloaders")
@@ -9,6 +10,7 @@ const TOKEN_EXPIRATION_TIME = 1000 * 60 * 30 // half an hour
 const schema = require("./schema")
 const typeDefs = gql(schema)
 const resolvers = require("./resolvers")
+const plugins = [ApolloServerPluginLandingPageGraphQLPlayground()]
 const context = async ({ req }) => {
   const getUserDataFromReq = () => {
     const authHeader = req.headers?.authorization
@@ -27,4 +29,5 @@ const context = async ({ req }) => {
   return { createToken, getUserDataFromReq, validateISBN, loaders }
 }
 
-module.exports = { typeDefs, resolvers, context }
+module.exports = { typeDefs, resolvers, context, plugins }
+
